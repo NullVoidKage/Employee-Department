212bbc1f6d68282b0de866c2dbb6760dda6d4cd2
NullVoidKage <ferwelo.n.bscs@gmail.com>
2024-08-29 18:14:40 +0800

    Added

--- src/main/java/com/example/demo/repository/DepartmentRepository.java
+++ src/main/java/com/example/demo/repository/DepartmentRepository.java
@@ -2,8 +2,12 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.DepartmentEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DepartmentRepository extends JpaRepository<DepartmentEntity, Long> {
+
+    @Query(value = "SELECT name FROM Employee", nativeQuery = true)
+    String getString();
 }
